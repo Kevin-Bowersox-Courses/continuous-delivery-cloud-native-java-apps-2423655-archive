9dd6da2f7ad50a2fd73d2cabccaa417e4257d4aa
kmb385 <kmb385@gmail.com>
2021-10-22 09:45:01 -0400

    Update setup_docker.sh

--- lab-prep/vagrant/provisioners/setup_docker.sh
+++ lab-prep/vagrant/provisioners/setup_docker.sh
@@ -13,7 +13,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
+sudo apt-get update
+
 #Install Docker
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
