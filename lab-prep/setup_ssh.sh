9f1b4149936c3e6c9a2f86c53162b13f0b3ae83b
kevin <kmb385@gmail.com>
2021-10-25 23:30:51 +0000

    updated

--- lab-prep/setup_ssh.sh
+++
@@ -1,6 +0,0 @@
-#Generate the SSH Key
-runuser -u vagrant -- ssh-keygen -t rsa -C "test@gmail.com" -q -P "" -f "/home/vagrant/.ssh/id_rsa"
-
-cat /home/vagrant/.ssh/id_rsa > /home/vagrant/private.key
-cat /home/vagrant/.ssh/id_rsa.pub > /home/vagrant/public.key
-
