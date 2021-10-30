484e5c92c0855947181a18654f6fdbcde9f066dd
Kevin Bowersox <kmb385@gmail.com>
2021-10-30 14:41:18 +0000

    Initial Setup

---
+++ playground/src/main/java/com/linkedin/javacd/Application.java
@@ -0,0 +1,11 @@
+package com.linkedin.javacd;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.capitalize("hello linkedin learning"));
+	}
+
+}
